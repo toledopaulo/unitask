65cc4c67630dc5a8537e1cd7d4c508f646591bf9
Paulo <paulo.aff1803@gmail.com>
2024-06-20 10:22:14 -0300

    feat: rotas das paginas alteradas para funcionar com flask

--- src/app.py
+++ src/app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, request, make_response, redirect, session, url_for, flash
 from flask_cors import CORS
-import dotenv
+from dotenv import load_dotenv
+import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
+load_dotenv()
 
+
+@app.route("/", methods=["GET"])
+def index():
+  return render_template("index.html")
+
+@app.route("/criar-conta", methods=["GET", "POST"])
+def criar_conta():
+  if request.method == "GET":
+    return render_template("crie_conta.html")
+  elif request.method == "POST":
+    return
+if __name__ in "__main__":
+  app.run()
